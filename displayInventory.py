7f16472a589fdf36f57007498dcec9350eae5340
Abbas Dargahi <adargahi97@gmail.com>
2022-04-15 12:42:27 -0500

    assignment 9.1

--- displayInventory.py
+++ displayInventory.py
@@ -3,7 +3,7 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     count = 0
 
-    for item, val in inventory.items():
+    for item, val in reversed(inventory.items()):
         print(val, item)
         count += int(val)
     print("Total number of items:", count)
